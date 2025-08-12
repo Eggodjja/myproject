713e1a13e73838dd06ee4afd1b57cf320bc43daa
root <root@Eggodjja.localdomain>
2025-08-12 14:26:18 +0300

    add everything without E2E tests

--- app/config.py
+++ app/config.py
@@ -9,13 +9,7 @@ class Config(object):
     HOST = os.environ.get('POSTGRES_HOST')
     PORT = os.environ.get('POSTGRES_PORT')
     DB = os.environ.get('POSTGRES_DB')
-<<<<<<< HEAD
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-fallback-secret')
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-=======
     DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
     SQLALCHEMY_DATABASE_URI = DATABASE_URI
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-fallback-secret')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
->>>>>>> 677dfb4 (add everything without E2E tests)
